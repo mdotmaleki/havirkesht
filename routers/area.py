c21277990a13a274c05369c3f21b38c80ab15c90
MdotMaleki <mdotmaleki@gmail.com>
2026-02-10 09:34:18 +0330

    Edit API And Push via CI/CD

--- routers/area.py
+++ routers/area.py
@@ -10,7 +10,8 @@ router = APIRouter(
 )
 
 @router.post("/provinces/", response_model=schemas.Province)
-def create_province(province: schemas.ProvinceCreate, db: Session = Depends(get_db)):
+def create_province(province: schemas.ProvinceCreate, db: Session = Depends(get_db),
+                    current_user: schemas.User = Depends(get_current_user)):
     return crud.create_province(db=db, province=province)
 
 @router.get("/provinces/{province_id}", response_model=schemas.Province)
